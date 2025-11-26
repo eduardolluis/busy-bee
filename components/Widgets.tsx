e66b58c116f4fa54277a28e6bb213479f74642c9
eduardolluis <1132907@est.intec.edu.do>
2025-11-26 13:06:21 -0400

    creating searching bar

--- components/Widgets.tsx
+++ components/Widgets.tsx
@@ -1,7 +1,16 @@
-export default function Widgets(){
-    return (
-        <div>
-            widgets
-        </div>
-    )
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+
+export default function Widgets() {
+  return (
+    <div className="p-3">
+      <div className=" bg-[#EFF3F4] text-[#89959D] h-[44px] flex items-center space-x-3 rounded-full pl-5">
+        <MagnifyingGlassIcon className="w-[20px] h-[20px]" />
+        <input
+          type="text"
+          placeholder="Search Busy Bee"
+          className="bg-transparent outline-none"
+        />
+      </div>
+    </div>
+  );
 }
