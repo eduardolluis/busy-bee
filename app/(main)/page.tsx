4948778392fe7592b1ab5788a23fb207262397c8
eduardolluis <1132907@est.intec.edu.do>
2025-11-26 19:46:20 -0400

    adding signup prompt

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,17 +1,22 @@
 import PostFeed from "@/components/PostFeed";
 import Sidebar from "@/components/Sidebar";
+import SignUpPrompt from "@/components/SignUpPrompt";
 import Widgets from "@/components/Widgets";
 
 export default function Home() {
   return (
-    <div
-      className="text-[#0F1419] min-h-screen 
+    <>
+      <div
+        className="text-[#0F1419] min-h-screen 
         max-w-[1400px] mx-auto flex justify-center
         "
-    >
-      <Sidebar />
-      <PostFeed />
-      <Widgets />
-    </div>
+      >
+        <Sidebar />
+        <PostFeed />
+        <Widgets />
+      </div>
+
+      <SignUpPrompt />
+    </>
   );
 }
