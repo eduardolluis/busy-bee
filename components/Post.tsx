ddc7bdd54f14ba92867850aaa2a61e685e3b5b73
eduardolluis <1132907@est.intec.edu.do>
2025-11-26 12:32:52 -0400

    styling post header

--- components/Post.tsx
+++ components/Post.tsx
@@ -10,13 +10,24 @@ export default function Post() {
 
 export function PostHeader() {
   return (
-    <div className="flex p-3">
+    <div className="flex p-3 space-x-5">
       <Image
         src="/profile-pic.png"
         width={44}
         height={44}
         alt="Profile Picture"
+        className="w-11 h-11"
       />
+      <div className="text-[15px] flex flex-col space-y-1.5">
+        <div className="flex space-x-1.5  text-[#707E89]">
+          <span className="font-bold text-[#0F1419]">Guest</span>
+          <span>@guest0000032</span>
+          <span>·</span>
+          <span>a day ago</span>
+        </div>
+
+        <span> asdasdsadasdads</span>
+      </div>
     </div>
   );
 }
