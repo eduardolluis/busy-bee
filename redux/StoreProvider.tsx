028ed2ed4fdbb5d03528f1916ebeac4d4c1c0f79
eduardolluis <1132907@est.intec.edu.do>
2025-11-26 20:18:41 -0400

    setting up redux store

---
+++ redux/StoreProvider.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { Provider } from "react-redux";
+import { store } from "./store";
+
+interface StoreProviderProps {
+  children: React.ReactNode;
+}
+
+export default function StoreProvider({ children }: StoreProviderProps) {
+  return (
+    <div>
+      <Provider store={store}>{children}</Provider>
+    </div>
+  );
+}
